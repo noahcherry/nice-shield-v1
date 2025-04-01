5d0a01f7b1705d432768dbb3618c5e1f21d93740
noah-cherry <noahcherry77@gmail.com>
2025-04-01 16:22:28 -0600

    rename structs and variables

--- boards/shields/nice_shield_v1/widgets/tamocatchi.c
+++ boards/shields/nice_shield_v1/widgets/tamocatchi.c
@@ -15,49 +15,49 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/events/wpm_state_changed.h>
 #include <zmk/wpm.h>
 
-#include "luna.h"
+#include "tamogatchi.h"
 
 #define SRC(array) (const void **)array, sizeof(array) / sizeof(lv_img_dsc_t *)
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-LV_IMG_DECLARE(dog_sit1_90);
-LV_IMG_DECLARE(dog_sit2_90);
-LV_IMG_DECLARE(dog_walk1_90);
-LV_IMG_DECLARE(dog_walk2_90);
-LV_IMG_DECLARE(dog_run1_90);
-LV_IMG_DECLARE(dog_run2_90);
-LV_IMG_DECLARE(dog_sneak1_90);
-LV_IMG_DECLARE(dog_sneak2_90);
+LV_IMG_DECLARE(penguin_sit1_90);
+LV_IMG_DECLARE(penguin_sit2_90);
+LV_IMG_DECLARE(penguin_walk1_90);
+LV_IMG_DECLARE(penguin_walk2_90);
+LV_IMG_DECLARE(penguin_run1_90);
+LV_IMG_DECLARE(penguin_run2_90);
+LV_IMG_DECLARE(penguin_sneak1_90);
+LV_IMG_DECLARE(penguin_sneak2_90);
 
 // #define ANIMATION_SPEED_IDLE 10000
 #define ANIMATION_SPEED_IDLE 960
 const lv_img_dsc_t *idle_imgs[] = {
-    &dog_sit1_90,
-    &dog_sit2_90,
+    &penguin_sit1_90,
+    &penguin_sit2_90,
 };
 
 // #define ANIMATION_SPEED_SLOW 2000
 #define ANIMATION_SPEED_SLOW 200
 const lv_img_dsc_t *slow_imgs[] = {
-    &dog_walk1_90,
-    &dog_walk2_90,
+    &penguin_walk1_90,
+    &penguin_walk2_90,
 };
 
 // #define ANIMATION_SPEED_MID 500
 #define ANIMATION_SPEED_MID 200
 const lv_img_dsc_t *mid_imgs[] = {
-    &dog_walk1_90,
-    &dog_walk2_90,
+    &penguin_walk1_90,
+    &penguin_walk2_90,
 };
 
 #define ANIMATION_SPEED_FAST 200
 const lv_img_dsc_t *fast_imgs[] = {
-    &dog_run1_90,
-    &dog_run2_90,
+    &penguin_run1_90,
+    &penguin_run2_90,
 };
 
-struct luna_wpm_status_state {
+struct tamogatchi_wpm_status_state {
     uint8_t wpm;
 };
 
@@ -69,7 +69,7 @@ enum anim_state {
     anim_state_fast
 } current_anim_state;
 
-static void set_animation(lv_obj_t *animing, struct luna_wpm_status_state state) {
+static void set_animation(lv_obj_t *animing, struct tamogatchi_wpm_status_state state) {
     if (state.wpm < 15) { // def: 5
         if (current_anim_state != anim_state_idle) {
             lv_animimg_set_src(animing, SRC(idle_imgs));
@@ -105,30 +105,30 @@ static void set_animation(lv_obj_t *animing, struct luna_wpm_status_state state)
     }
 }
 
-struct luna_wpm_status_state luna_wpm_status_get_state(const zmk_event_t *eh) {
+struct tamogatchi_wpm_status_state tamogatchi_wpm_status_get_state(const zmk_event_t *eh) {
     struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(eh);
-    return (struct luna_wpm_status_state){.wpm = ev->state};
+    return (struct tamogatchi_wpm_status_state){.wpm = ev->state};
 };
 
-void luna_wpm_status_update_cb(struct luna_wpm_status_state state) {
-    struct zmk_widget_luna *widget;
+void tamogatchi_wpm_status_update_cb(struct tamogatchi_wpm_status_state state) {
+    struct zmk_widget_tamogatchi *widget;
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_animation(widget->obj, state); }
 }
 
-ZMK_DISPLAY_WIDGET_LISTENER(widget_luna, struct luna_wpm_status_state, luna_wpm_status_update_cb,
-                            luna_wpm_status_get_state)
+ZMK_DISPLAY_WIDGET_LISTENER(widget_tamogatchi, struct tamogatchi_wpm_status_state, tamogatchi_wpm_status_update_cb,
+                            tamogatchi_wpm_status_get_state)
 
-ZMK_SUBSCRIPTION(widget_luna, zmk_wpm_state_changed);
+ZMK_SUBSCRIPTION(widget_tamogatchi, zmk_wpm_state_changed);
 
-int zmk_widget_luna_init(struct zmk_widget_luna *widget, lv_obj_t *parent) {
+int zmk_widget_tamogatchi_init(struct zmk_widget_tamogatchi *widget, lv_obj_t *parent) {
     widget->obj = lv_animimg_create(parent);
     lv_obj_center(widget->obj);
 
     sys_slist_append(&widgets, &widget->node);
 
-    widget_luna_init();
+    widget_tamogatchi_init();
 
     return 0;
 }
 
-lv_obj_t *zmk_widget_luna_obj(struct zmk_widget_luna *widget) { return widget->obj; }
+lv_obj_t *zmk_widget_tamogatchi_obj(struct zmk_widget_tamogatchi *widget) { return widget->obj; }
